d6ab1d8c7d049474cea8762e8d432f90c986c3ef
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2024-11-27 06:43:00 -0800

    first commit

---
+++ src/main/java/com/javatar/paxosconsensusalgorithms/PaxosConsensusAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.javatar.paxosconsensusalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaxosConsensusAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaxosConsensusAlgorithmsApplication.class, args);
+    }
+
+}
